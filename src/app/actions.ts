17f0d755ab59d61a41c15e6051a024121f0d2dab
Paul Kavaleuski <kavalbusiness@gmail.com>
2025-03-27 23:30:27 -0400

    Remove unused imports

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,8 +1,5 @@
 'use server';
 
-import { revalidatePath } from 'next/cache';
-import { redirect } from 'next/navigation';
-
 import { Resend } from 'resend';
 
 export async function submitForm(formData: FormData) {
